13d79b16365aa52798d31eec940812e25f980b28
Amelbo3 <amelbo3@gmail.com>
2025-04-30 21:32:37 -0400

    complete template: FastAPI + SQLModel
    Signed-off-by: Amelbo3 <amelbo3@gmail.com>

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from database import *
 
 app = FastAPI()
 
+from routes import *
 
 @app.get("/")
 def read_root():
@@ -19,5 +20,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
 
 
